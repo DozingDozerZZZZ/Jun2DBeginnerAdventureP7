11f021325bd6940275c373354822a21bea2bfbfc
DozingDozerZZZZ <763664@student.dusd.net>
2025-11-18 13:58:03 -0800

    Updated charcter movement and chnaged player character

--- P7Jun2DBeginnerAdventure/Assets/scripts/PlayerController.cs
+++ P7Jun2DBeginnerAdventure/Assets/scripts/PlayerController.cs
@@ -5,46 +5,23 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public InputAction MoveAction;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        MoveAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
+
     {
-        float horizonal = 0.0f;
-        if (Keyboard.current.leftArrowKey.isPressed)
-        {
 
-            horizonal = -1.0f;
-
-        }
-        else if (Keyboard.current.rightArrowKey.isPressed)
-        {
-
-            horizonal = 1.0f;
-
-        }
-
-        Debug.Log(horizonal);
-
-
-        float vertical = 0.0f;
-        if (Keyboard.current.upArrowKey.isPressed)
-        {
-            vertical = 1.0f;
-        }
-        else if (Keyboard.current.downArrowKey.isPressed)
-        {
-            vertical = -1.0f;
-        }
-        Debug.Log(vertical);
-
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizonal;
-        position.y = position.y + 0.1f * vertical;
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+        Vector2 position = (Vector2)transform.position + move * 0.1f;
         transform.position = position;
+
     }
 }
